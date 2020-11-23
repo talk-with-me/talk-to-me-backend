1cfe64385ec1b38b95696735824fbe054888ec09
Andrew Zhu <me@andrew-zhu.com>
2020-11-23 09:44:39 -0800

    implement basic DialoGPT replier
    todo: history

--- scripts/setup-gpt2.py
+++ scripts/setup-gpt2.py
@@ -1,13 +1,8 @@
 """
 Downloads the GPT-2 model for use in bot replies. Used in a build step to provide the model.
 """
-import os
-
-import gpt_2_simple as gpt2
-
-model_name = "124M"
+from transformers import AutoTokenizer, AutoModelWithLMHead
 
 if __name__ == '__main__':
-    if not os.path.isdir(os.path.join("models", model_name)):
-        print(f"Downloading {model_name} model...")
-        gpt2.download_gpt2(model_name=model_name)  # model is saved into current directory under /models/124M/
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium", cache_dir='models')
+    model = AutoModelWithLMHead.from_pretrained("microsoft/DialoGPT-medium", cache_dir='models')
