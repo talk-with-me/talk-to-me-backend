07f73b8895afa1fc88587ad42e31ecca1ad555c8
Tyler Hoang <tylerhoang@Tylers-MacBook-Air.local>
2020-12-01 00:56:05 -0800

    chore: addressed some pr comments

--- admin.py
+++ admin.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 from flask_pymongo import PyMongo
@@ -10,19 +11,22 @@ from bson import json_util, ObjectId
 from lib import errors
 from lib.utils import clean_json, error, expect_json, success
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "ttmadmin"
-CORS(app)
+admin = Flask(__name__)
+admin.config["SECRET_KEY"] = "ttmadmin"
+CORS(admin)
 mdb = MongoClient(db.MONGO_URL).db
 
-@app.route("/auth", methods=["POST"])
-@expect_json(password=str)
-def admin_auth(body):
-    if(body['password'] == "ttmadmin"):
-        return success("Authorized")
-    return error(403, "Incorrect password")
+def requires_auth(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        token = request.headers.get('authorization')
+        if(token != "ttmadmin"):
+            return error(403, "Unauthorized")
+        return f(*args, **kwargs)
+    return decorated_function
 
-@app.route("/reports", methods=["GET"])
+@admin.route("/reports", methods=["GET"])
+@requires_auth
 def get_reports():
     reports = mdb.reports.find()
     data=[]
@@ -30,20 +34,21 @@ def get_reports():
         x['_id'] = str(x['_id'])
         data.append(x)
     jsonData=json.dumps(data)
-    return jsonData
+    return success(jsonData)
 
-@app.route("/reportmessages", methods=["POST"])
-@expect_json(room_id=str)
-def get_reported_messages(body):
-    conversation = mdb.reported_messages.find({'room_id' : body['room_id']})
+@admin.route("/reports/<room_id>/messages", methods=["GET"])
+@requires_auth
+def get_reported_messages(room_id):
+    conversation = mdb.reported_messages.find({'room_id' : room_id})
     data=[]
     for x in conversation:
         x['_id'] = str(x['_id'])
         data.append(x)
     jsonData=json.dumps(data)
-    return jsonData
+    return success(jsonData)
 
-@app.route("/banuser", methods=["POST"])
+@admin.route("/banuser", methods=["POST"])
+@requires_auth
 @expect_json(room_id=str, reason=str)
 def ban_user(body):
     report = mdb.reports.find_one({'room_id' : body['room_id']})
@@ -61,7 +66,8 @@ def ban_user(body):
     mdb.bannedUsers.insert_one(ban_object)
     return success("Reported user banned")
 
-@app.route("/bannedusers", methods=["GET"])
+@admin.route("/bannedusers", methods=["GET"])
+@requires_auth
 def get_banned_users():
     banned_users = mdb.bannedUsers.find()
     data=[]
@@ -69,13 +75,14 @@ def get_banned_users():
         x['_id'] = str(x['_id'])
         data.append(x)
     jsonData=json.dumps(data)
-    return jsonData
+    return success(jsonData)
 
-@app.route("/unbanuser", methods=["POST"])
+@admin.route("/unbanuser", methods=["POST"])
+@requires_auth
 @expect_json(ip=str)
 def unban_user(body):
     mdb.bannedUsers.delete_one({"ip" : body['ip']})
     return success("User unbanned")
 
 if __name__ == "__main__":
-    app.run(port=6000, host="0.0.0.0")
+    admin.run(port=6000, host="0.0.0.0")
